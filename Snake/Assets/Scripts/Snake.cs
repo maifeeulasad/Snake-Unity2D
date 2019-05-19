2702c4a3471b742c522abbb3d32496b41b9bc591
maifeeulasad <maifeeulasad@gmail.com>
2019-05-19 13:41:40 +0600

    update

---
+++ Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+
+    public GameObject snakeCell;
+    List<GameObject> cells = new List<GameObject>();
+
+    
+    
+    void LateUpdate()
+    {
+        foreach(Transform child in transform)
+        {
+            cells.Add(child.gameObject);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            cells[0].transform.position
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag.Equals("food"))
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+}
